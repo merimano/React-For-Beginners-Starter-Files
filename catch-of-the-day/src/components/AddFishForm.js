81fbd4351fa0d2779b03e951f73e959f3be11ead
merimano <meriem.manouchi@gmail.com>
2021-12-17 08:03:49 +0100

    React fish instances done

---
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+class AddFishForm extends React.Component {
+    // Create a ref for each reference
+    nameRef = React.createRef();
+    priceRef = React.createRef();
+    statusRef = React.createRef();
+    descRef = React.createRef();
+    imageRef = React.createRef();
+    // Create an arrow function to enable this to be the created instance.
+    createFish = (event) => {
+         // 1. Stop the form from submitting
+        event.preventDefault();
+        // 2. Get the text from the form and create a fish instance
+        const fish = {
+            name: this.nameRef.current.value,
+            price: this.priceRef.current.value,
+            status: this.statusRef.current.value,
+            desc: this.descRef.current.value,
+            image: this.imageRef.current.value
+        };
+        console.log(fish);
+    }
+
+   
+    
+    // 3. Create a new instance of a fish and update the page.
+
+    render() {
+        return (
+            // Add an inline eventlistener
+            <form className="fish-edit" onSubmit={this.createFish}>
+               <input name="name" ref={this.nameRef} type="text" placeholder="Name" />
+               <input name="price" ref={this.priceRef} type="text" placeholder="Price"/>
+               <select name="status" ref={this.statusRef} type="text" placeholder="Status">
+                   <option value="availaible">Fresh!</option>
+                   <option value="unavailiable">Sold out!</option>
+               </select>
+               <textarea name="desc" ref={this.descRef} type="text" placeholder="Desc"/>
+               <input name="image" ref={this.imageRef} type="text" placeholder="Image"/>
+               <button type="submit">Add Fish</button>
+            </form>
+        );
+    }
+}
+
+export default AddFishForm;
--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+    // Create a "ref" in order to access the item in the DOM.
+    myInput = React.createRef();
+    
+    // Declare a property (goToStore) and set it to an arrowfunction since the property is bound to the instance which enables the access to "this".
+    goToStore = event => {
+        // 1. Stop the event from submitting.
+        event.preventDefault();
+        // 2. get the text from that input.
+        const storeName = this.myInput.current.value;
+        // 3. Change the page to store
+        this.props.history.push(`/store/${storeName}`);
+    };
     render() {
         return (
-            <form className="store-selector">
+            // Create a form with an inline eventhandler onSubmit, and tell it which function to run. This works directly when someone submits the form.
+            <form className="store-selector" onSubmit={this.goToStore}> 
                 <h2>Please enter a store</h2>
-                <input type="text"required placeholder="Store Name"></input>
+                <input 
+                    type="text"
+                    // this.myInput links up to createRef and enables the selection of it.
+                    ref={this.myInput}
+                    required placeholder="Store Name"
+                    defaultValue={getFunName()}
+                    />
                 <button type="submit">Visit Store</button>
             </form>
             );
